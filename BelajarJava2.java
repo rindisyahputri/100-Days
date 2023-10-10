c8c6f42dc3af106bb51b98e17cfd79f62010cc52
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-10 16:25:56 +0800

    Hari Kedua

---
+++ BelajarJava2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    String nama = "rindi";
+    String alamat = "kalimbua";
+    int umur = 18;
+    String Hp = "081240001779";
+    char jenis = 'p';
+  
+    System.out.println(nama);
+    System.out.println(alamat);
+    System.out.println (umur);
+    System.out.println(Hp);
+    System.out.println(jenis);
+   }
+ }
