5835d9f60f146cc6be116a24c2f5d66ec00654e0
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-12 18:12:44 +0800

    Create Rindiiii day4.java

---
+++ Rindiiii day4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int angka=3;
+        
+        if(angka==3){
+         System.out.println ("salah");
+         }else if(angka>4){
+           System.out.println("benar");
+           
+           }else{
+           
+           System.out.println("santui");
+           }
+   }
+ }
