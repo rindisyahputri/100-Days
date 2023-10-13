632eb46212f54b45499a98aa3771fc7d80df0fca
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-13 23:55:26 +0800

    Create Rindiiday5.java

---
+++ Rindiiday5.java
@@ -0,0 +1,13 @@
+package rindi;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int nilai = 2 ;
+        int angka = 3;
+        System.out.println(angka==nilai || angka!=nilai);
+        System.out.print(angka==nilai && angka!=nilai);
+    }
+    
+}
