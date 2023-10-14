31b32c44e59c9d7f29bf982b3842c15b94067ca2
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-14 17:15:47 +0800

    Create Rindiiday6.java

---
+++ Rindiiday6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+   /*
+      Tipe Data Angka Bilangan Berkoma
+       Floating Point           -> float    -> 32 bit
+       Double Floating Point    -> double   -> 54 bit
+    */
+
+    float berat = 12.5f;
+    double tinggi = 167.67;
+    
+    System.out.println("Berat = " + berat + " kg");
+    System.out.println("Tinggi = " + tinggi + " cm");
+
+   }
+ }
