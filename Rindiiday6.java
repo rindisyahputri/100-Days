666527fbc85650ab144d05f37f3d81e0cc08a1dc
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-14 20:07:46 +0800

    Rindiiday 6.java

--- Rindiiday6.java
+++ Rindiiday6.java
@@ -1,19 +1,14 @@
 import java.util.*;
 
- // Compiler version JDK 11.0.2
 
  class Dcoder
  {
    public static void main(String args[])
    { 
-   /*
-      Tipe Data Angka Bilangan Berkoma
-       Floating Point           -> float    -> 32 bit
-       Double Floating Point    -> double   -> 54 bit
-    */
+  
 
-    float berat = 12.5f;
-    double tinggi = 167.67;
+    float berat = 15.5f;
+    double tinggi = 345.67;
     
     System.out.println("Berat = " + berat + " kg");
     System.out.println("Tinggi = " + tinggi + " cm");
