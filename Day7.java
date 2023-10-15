41a8363f2898db88ed003e8ec7aa451781c694e9
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-15 21:48:35 +0800

    day 7

---
+++ Day7.java
@@ -0,0 +1,10 @@
+package day.pkg7;
+public class Day7 {
+    public static void main(String[] args) {
+          int num = 4;
+        for(int i = 1; i <= 10; ++i)
+        
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+    }
+    
+}
