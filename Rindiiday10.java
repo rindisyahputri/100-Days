bb4e8092203fcbc5ab24ac4c02bb5373776d2b2e
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-18 22:09:03 +0800

    Pecahan

---
+++ Rindiiday10.java
@@ -0,0 +1,11 @@
+public class VariabelSederhana {
+    public static void main(String[] args) {
+        int angka = 5;
+        double bilanganPecahan = 3.14;
+        String teks = "Ini adalah contoh variabel";
+
+        System.out.println("Angka: " + angka);
+        System.out.println("Bilangan Pecahan: " + bilanganPecahan);
+        System.out.println("Teks: " + teks);
+    }
+}
