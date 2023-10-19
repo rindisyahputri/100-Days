08a11a75349952d6787b04112ba4804f159ada9b
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-19 16:53:33 +0800

    Percabangan day 11

---
+++ Rindii day 11.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     int a = 2, b = 2;
+     String jawaban;
+     
+     jawaban = a > b ? "iya" : "tidak";
+    System.out.println(jawaban);
+   }
+ }
