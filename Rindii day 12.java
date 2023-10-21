21631c415c7e4691b8914f487ab12f79f4e57c5e
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-21 09:00:47 +0800

    Perulangan

---
+++ Rindii day 12.java
@@ -0,0 +1,6 @@
+{public static void main(String[] args) {
+System.out.println("1 Random Number: " + Math.random());   
+System.out.println("2 Random Number: " + Math.random());  
+System.out.println("3 Random Number: " + Math.random());    
+}   
+}
