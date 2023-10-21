a9d7bb2666843c4148941a7270f2c45359785649
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-21 09:18:00 +0800

    Update Rindii day 12.java

--- Rindii day 12.java
+++ Rindii day 12.java
@@ -1,6 +1,7 @@
-{public static void main(String[] args) {
-System.out.println("1 Random Number: " + Math.random());   
-System.out.println("2 Random Number: " + Math.random());  
-System.out.println("3 Random Number: " + Math.random());    
-}   
+public class pia {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("angka-" + i);
+        }
+    }
 }
