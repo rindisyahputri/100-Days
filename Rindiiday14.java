2a1fd09982e09c267bc9e9c5b97605abd6b5a990
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-22 13:46:05 +0800

    Percabangan if else

---
+++ Rindiiday14.java
@@ -0,0 +1,19 @@
+package rindi;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 3;
+        
+        if(angka>3){
+            System.out.println("salah");
+        }else if(angka>4){
+          System.out.println("benar") ; 
+        
+        }else
+            
+        System.out.println("santui");
+    }
+        
+    
+    }
