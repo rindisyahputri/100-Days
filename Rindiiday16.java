1822a758f89f64ad5f61ffb924f331861ef0d5dc
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-24 22:40:01 +0800

    Day 16

---
+++ Rindiiday16.java
@@ -0,0 +1,17 @@
+package days16;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        System.out.print("masukkan jumlah looping : ");
+        int loop = SC.nextInt();
+        for (int i = 0; i < loop; i++) {
+        
+        System.out.println(i);
+    }
+    
+}
+}
