ff727e56c2104adfcb786fa14433dee687fe8f7a
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-25 22:41:55 +0800

    Create Rindi day17

---
+++ Rindi day17.java
@@ -0,0 +1,17 @@
+package days17;
+
+import java.util.Scanner; // import the Scanner class
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    Scanner myObj = new Scanner(System.in);
+String userName;
+
+// Enter username and press Enter
+
+System.out.println("Username");
+userName = myObj.nextLine();
+System.out.println("Username is : " + userName);
+}
+}
