99102f7942a1a78934907de8308fdec49df88094
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-26 22:47:33 +0800

    Coding day18

---
+++ Rindii day18.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        {        
+     boolean myBool = true;       
+    String myText = "false";   
+    System.out.println(myBool);
+    System.out.println(myText);
+
+} 
+
+    }
+}
