5c38e7f8c7a6359cd8b3ce836fd58ad4d6ea4921
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-28 01:07:42 +0800

    Create Rindiiday 19..java

---
+++ Rindiiday 19..java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int angka = 5;
+String teks = "Halo, dunia!";
+double bilanganDesimal = 3.14;
+
+System.out.println(angka); 
+System.out.println(teks);  
+System.out.println(bilanganDesimal);
+	}
+}
