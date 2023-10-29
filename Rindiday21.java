9eddd8fa4308f9ea8b01f6dc296a29c8eb5a8c46
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-30 00:05:03 +0800

    Rindiday21

---
+++ Rindiday21.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+       
+         int nilai = 6;
+        int angka = 3;
+        System.out.println(angka=angka);
+        System.out.println(angka!=angka);
+ 
+       }
+    
+}
