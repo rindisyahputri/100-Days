8b218f7958a4d4fa969f6fd4a4ed901135ed450d
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-10-30 23:34:29 +0800

    Create Rindiiday22.java

---
+++ Rindiiday22.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+		
+static void myMethod() {
+System.out.println("  1 adalah bilangan asli");
+}
+
+
+public static void main(String[] args) {
+    
+    myMethod();
+}
+}
+
