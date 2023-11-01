f6cb855964c7a78da0998353dcbbb0d3443427bd
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-02 00:33:21 +0800

    Array

---
+++ Rindiiiday24.java
@@ -0,0 +1,14 @@
+// Mendefinisikan sebuah array integer dengan panjang 5
+        int[] Nilai = new int[5];
+
+        // Mengisi array dengan nilai
+        Nilai[0] = 10;
+        Nilai[1] = 20;
+        Nilai[2] = 30;
+        Nilai[3] = 40;
+        Nilai[4] = 50;
+
+        // Mencetak isi array
+        for (int i = 0; i < Nilai.length; i++) {
+            System.out.println("Element ke-" + i + ": " + Nilai[i]);
+        }
