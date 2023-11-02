82f64d745ffa08a1dac5147c01acb7b0bd494eb3
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-02 21:42:40 +0800

    Percabangan

---
+++ Rindiiday25.java
@@ -0,0 +1,13 @@
+public class Java {
+    public static void main(String[] args) {
+       
+        int angka = 5;
+        double pecahan = 3.14;
+        String teks = "Ini adalah contoh variabel";
+
+        System.out.println("Nilai angka: " + angka);
+        System.out.println("Nilai pecahan: " + pecahan);
+        System.out.println("Nilai teks: " + teks);
+
+    }
+}
