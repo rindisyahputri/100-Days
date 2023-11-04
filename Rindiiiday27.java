d3d3c6e12ec619772558828a4a0e8029a3fe96da
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-04 18:14:23 +0800

    Create Rindiiiday27.java

---
+++ Rindiiiday27.java
@@ -0,0 +1,16 @@
+package days27;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        Scanner data = new Scanner(System.in);
+        int angka1 = data.nextInt();
+        int angka2 = data.nextInt();
+        System.out.println(angka1);
+        System.out.println(angka2);
+        
+    }
+
+    }
