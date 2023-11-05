244e9f19cf8c2bbe0dc30edf59bb4fb34d761e54
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-05 23:21:18 +0800

    Percabangan

---
+++ Rindiday28.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int nilai = 50;
+        int angka = 20;
+        
+        
+        System.out.println(nilai>angka);
+        System.out.println(nilai%angka);
+        System.out.println(nilai<angka);
+        System.out.println(nilai*angka);
+    }
+    
+}
