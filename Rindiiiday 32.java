d70ba87d201ef68ef11630193cb48e8f99895e90
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-09 23:12:02 +0800

    Create Rindiiiday 32.java

---
+++ Rindiiiday 32.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int angka = 10;
+
+if (angka > 10) {
+    System.out.println("Angka lebih besar dari 10");
+} else if (angka < 10) {
+    System.out.println("Angka lebih kecil dari 10");
+} else {
+    System.out.println("Angka sama dengan 10");
+    
+   
+}
+	}
+}
