6fe2d11dd4cb28dba8126e72000ca68d5627cdb4
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-13 19:25:41 +0800

    Percabangan logika

---
+++ Rindiiiday 36.java
@@ -0,0 +1,21 @@
+package belajar.coding;
+
+public class BelajarCoding {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        
+        if (a*b == 50   && a/b ==  10) {
+            System.out.println("a");
+            
+        }
+            
+        else{
+            System.out.println("b");
+        }
+                
+
+    }
+}
