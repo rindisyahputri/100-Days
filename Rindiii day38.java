019cafe0faf25e83922786e50ee224b7d4696516
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-15 23:08:47 +0800

    Create Rindiii day38.java

---
+++ Rindiii day38.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    
+        int angka1 = 5;
+        int angka2 = 8;
+
+        if (angka1 < angka2) {
+            System.out.println(angka1 + " lebih kecil dari " + angka2);
+        } else if (angka1 > angka2) {
+            System.out.println(angka1 + " lebih besar dari " + angka2);
+        } else {
+            System.out.println(angka1 + " sama dengan " + angka2);
+        
+    
+}
+   }
+ }
