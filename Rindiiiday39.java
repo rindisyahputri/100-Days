b38c69d61ebfa9c84aa5703a6c2da8de80956ec6
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-16 21:23:14 +0800

    Percabangan aritmatika

---
+++ Rindiiiday39.java
@@ -0,0 +1,13 @@
+public class Perkalian {
+    public static void main(String[] args) {
+        // Mendefinisikan dua angka untuk dikalikan
+        int angka1 = 5;
+        int angka2 = 8;
+
+        // Melakukan operasi perkalian
+        int hasilPerkalian = angka1 * angka2;
+
+        // Menampilkan hasil
+        System.out.println("Hasil perkalian: " + hasilPerkalian);
+    }
+}
