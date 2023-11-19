bbcb07e52f288e24b26aa6e7dfa8659161dcf84b
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-19 23:02:09 +0800

    Create Rindiiday42.java

---
+++ Rindiiday42.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    
+        int tinggi = 5; // Tinggi bisa di ubah ubah 
+        int lebar = 7; // Begitupun dengan lebarrr
+        
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= lebar; j++) {
+                System.out.print("∆ ");
+            }
+        System.out.println();
+        }
+    }
+
+   }
