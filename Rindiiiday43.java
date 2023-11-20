d99305b590a68f41d2869c4b33ef87aa657fc071
rindisyahputri <147327842+rindisyahputri@users.noreply.github.com>
2023-11-20 20:25:31 +0800

    Percabangan

---
+++ Rindiiiday43.java
@@ -0,0 +1,20 @@
+package percabangan;
+public class Percabangan {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=10;
+        switch (x){
+            case 1:
+                System.out.println("nilai x = 1");
+                break;
+            case 2:
+                System.out.println("nilai x = 2");
+                break;
+            case 3:
+                System.out.println("nilai x = 3");
+                break;
+            default:
+                System.out.println("nilai a tidak di katahui");
+        }
+    }
+}
